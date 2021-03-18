619c3bf96418a3ba871f23377719b4af484a97fc
absaGH <absa3852@gmail.com>
2021-03-18 11:51:08 +0000

    syntax corrected

--- holberton.h
+++ holberton.h
@@ -7,6 +7,6 @@ int _putchar(char chr);
 int _printf(const char *format, ...);
 int print_str(char *s);
 int print_char(char ch);
-
+int print_integer(int n);
 
 #endif
--- printf.c
+++ printf.c
@@ -15,7 +15,7 @@ va_list args;
 int length = 0;
 char *printstr;
 char printchar;
-int printInteger;
+/*int printInteger;*/
 
 if (format == NULL)
 return (-1);
