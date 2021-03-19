438ede8dba5081ba747baa45040121f8ed7e8ca0
Yosef-S-A <yosefsamuel22@gmail.com>
2021-03-19 08:34:01 +0000

    task

--- printstring.c
+++ printstring.c
@@ -23,65 +23,12 @@ return (1);
 int print_str(char *s)
 {
 int count;
-
+  
 if (s == NULL)
-s = "(null)";
+s = "";
 for (count = 0; s[count]; count++)
 {
 _putchar(s[count]);
-}
-return (count);
-}
-
-/**
- * print_reverse - displays a string reversed
- * @str: string to display
- *
- * Return: number of chars printed
- */
-int print_reversed(char *str)
-{
-int i, j, count = 0;
-
-for (i = 0; str[i]; i++)
-;
-for (j = i - 1; j >= 0; j--)
-{
-_putchar(str[j]);
-count++;
-}
-return (count);
-}
-
-/**
- * print_R - displays a string after applying rot13
- * @string: string to be converted into rot13
- *
- * Return: number of chars displayed
- */
-int print_R(char *str)/*newly added*/
-{
-unsigned int i, j;
-int count = 0;
-char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
-for (i = 0; str[i]; i++)
-{
-for (j = 0; in[j]; j++)
-{
-if (in[j] == str[i])
-{
-_putchar(rot13[j]);
-count++;
-break;
-}
-}
-if (!in[j])
-{
-_putchar(str[i]);
-count++;
-}
 }
 return (count);
 }
