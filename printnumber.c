f147e98a3f18b8d378f712c84509af1314fd40ae
absaGH <absa3852@gmail.com>
2021-03-19 15:44:07 +0000

    task 1

--- printnumber.c
+++ printnumber.c
@@ -6,7 +6,7 @@
  *
  * Return: number of chars and digits printed
  */
-int print_integer(int n)
+int print_integer(int n)/*newly added*/
 {
   int a[10];
   int j, m, sum, count;
@@ -19,134 +19,21 @@ int print_integer(int n)
       m /= 10;
       a[j] = (n / m) % 10;
     }
-  for (j = 0, sum = 0; j < 10; j++)
+  if (n < 0)
     {
-      sum += a[j];
-      if (sum != 0 || j == 9)
-	{
-	  _putchar('0' + a[j]);
-	  count++;
-	}
-    }
-  return (count);
-}
-
-/**
- * print_binary - takes an unsigned int and displays it in binary notation
- * @n: unsigned int to print
- *
- * Return: number of digits displayed
- */
-int print_binary(unsigned int n)
-{
-  int count;
-  int i = 0;    
-  int binaryNum[32]; 
-  
-  while (n > 0) 
-    { 
-      binaryNum[i] = n % 2; 
-      n = n / 2; 
-      i++; 
-    }   
-  for (i = i-1; i >= 0; i--)    
-    {   
-      _putchar('0'+binaryNum[i]);
-      count++; 
-    }    
-  return (count);
-}
-
-/**
- * print_unsigned_int - displays unsigned integer number
- * @n: integer to print
- *
- * Return: number of chars and digits printed
- */
-int print_unsigned_int(unsigned int n)/*newly added*/
-{
-  unsigned int a[10];
-  unsigned int j, m, sum, count;
-
-  count = 0;
-  m = 1000000000;
-  a[0] = n / m;
-  for (j = 1; j < 10; j++)
-    {
-      m /= 10;
-      a[j] = (n / m) % 10;
-    }
-  for (j = 0, sum = 0; j < 10; j++)
-    {
-      sum += a[j];
-      if (sum != 0 || j == 9)
-	{
-	  _putchar('0' + a[j]);
-	  count++;
-	}
-    }
-  return (count);
-}
-
-/**
- * print_octal - displays octal number
- * @n: integer to be printed as octal
- *
- * Return: number of chars and digits printed
- */
-int print_octal(unsigned int n)/*newly added*/
-{
-  unsigned int quotient;
-  int octalNumber[100], i = 1, j, count = 0;
- 
-  quotient = n;
-  while (quotient != 0)
-    {
-      octalNumber[i++] = quotient % 8;
-      quotient = quotient / 8;
-    }
-  for (j = i - 1; j > 0; j--)
-    {
-      _putchar('0' + octalNumber[j]);
+      _putchar('-');
       count++;
+      for (j = 0; j < 10; j++)
+	a[j] *= -1;
     }
-  return (count);
-}
-
-/**
- * print_hex - displays hexadecimal number
- * @decimalNum: integer to be printed in Hex
- * @c: variable to indicate whehter uppercase or lowercase should be used
- *
- * Return: number of chars and digits printed
- */
-int print_hex(int decimalNum, unsigned int c)/*newly added*/
-{
-  unsigned int remainder,quotient;
-  int count=0,j;
-  char hexadecimalNumber[100];
-  quotient = decimalNum;
-  while (quotient!=0) 
+  for (j = 0, sum = 0; j < 10; j++)
     {
-      remainder = quotient % 16;
-      if (c == 0)
+      sum += a[j];
+      if (sum != 0 || j == 9)
 	{
-	  if ( remainder < 10)
-	    remainder =remainder + 48; 
-	  else
-	    remainder = remainder + 87;
+	  _putchar('0' + a[j]);
+	  count++;
 	}
-      else
-	{
-	  if( remainder < 10)
-	    remainder =remainder + 48; 
-	  else
-	    remainder = remainder + 55;
-	}
-      hexadecimalNumber[count++]= remainder;
-      quotient = quotient / 16;
     }
-  for (j = count-1 ;j>= 0;j--)
-    _putchar(hexadecimalNumber[j]);
   return (count);
 }
