1680bedf387dd23422d1832fbf1e5ae04292d6f3
Yosef-S-A <yosefsamuel22@gmail.com>
2021-08-14 02:07:25 +0000

    puppet script to create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,11 +1,10 @@
 # Create a file in /tmp
 
-file {
-     'holberton':
-     ensure  => 'present',
-     path    => '/tmp/holberton',
-     mode    => '0744'
-     owner   => 'www-data'
-     group   => 'www-data'
-     content => 'I love Puppet'
+file { 'holberton':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  path    => '/tmp/holberton',
 }
