09efa85caaed056577e1323ddf842a5e3874c858
Yosef-S-A <yosefsamuel22@gmail.com>
2021-09-28 19:24:22 +0000

    pep8 compatible task

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """queries the subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        #"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        # "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
         "User-Agent": "linux:0x16.api.advanced"
 
     }
